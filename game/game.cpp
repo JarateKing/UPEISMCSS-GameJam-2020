f28f961338285ef06fade66238650d78368eb7d5
JarateKing <scmacintosh@upei.ca>
2020-02-08 12:20:31 -0400

    added display render setup

--- game/game.cpp
+++ game/game.cpp
@@ -2,6 +2,27 @@
 
 using namespace std;
 
+#define DISP_W 100
+#define DISP_H 30
+char** display;
+
+void render() {
+	for (int i = 0; i < DISP_H; i++) {
+		for (int j = 0; j < DISP_W; j++) {
+			cout << display[i][j];
+		}
+		cout << '\n';
+	}
+}
+
 int main() {
-	cout << "Hello World!";
+	display = new char*[DISP_H];
+	for (int i = 0; i < DISP_H; i++) {
+		display[i] = new char[DISP_W];
+		for (int j = 0; j < DISP_W; j++) {
+			display[i][j] = '.';
+		}
+	}
+	
+	render();
 }
--- game/run.bat
+++ game/run.bat
@@ -1,3 +1,4 @@
+MODE 101,31
 g++ -g -O2 -std=gnu++17 -static game.cpp 
 a.exe 
 pause
